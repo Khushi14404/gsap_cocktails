d0d8438ed67f4cc22519a9b5cc31d697fb05c378
Khushi <ks2004144@gmail.com>
2025-10-05 15:08:50 +0530

    implement hero section layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
     </main>
   );
 };
